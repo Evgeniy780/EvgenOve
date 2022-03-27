9d278e5eaf539177116699689608539b432bf16d
Evgeniy780 <atomanowv@gmail.com>
2022-03-27 16:41:46 +0300

    add ass.py

--- ass.py
+++ ass.py
@@ -1,9 +1,6 @@
 import pyttsx3
-
 engine = pyttsx3.init()     # инициализация движка
-
 engine.setProperty('rate', 150)     # скорость речи
 engine.setProperty('volume', 0.9)   # громкость (0-1)
-
 engine.say('Кто по пивку ? А?')
 engine.runAndWait()
